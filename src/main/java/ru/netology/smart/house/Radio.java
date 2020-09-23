0c51f8b3bfe53ae5a11ba499221a921e3066b2e5
Eleonora Popushoi <eleonorapopushoi@gmail.com>
2020-09-23 12:41:03 +0300

    Updated version of radio adjusted under lombok version

--- src/main/java/ru/netology/smart/house/Radio.java
+++ src/main/java/ru/netology/smart/house/Radio.java
@@ -48,22 +48,6 @@ public class Radio {
         currentRadioStation--;
     }
 
-    public int getMaxVolume() {
-        return maxVolume;
-    }
-
-    public void setMaxVolume(int maxVolume) {
-        this.maxVolume = maxVolume;
-    }
-
-    public int getMinVolume() {
-        return minVolume;
-    }
-
-    public void setMinVolume(int minVolume) {
-        this.minVolume = minVolume;
-    }
-
     public int getCurrentVolume() {
         return currentVolume;
     }
@@ -78,22 +62,6 @@ public class Radio {
             return;
         }
         this.currentVolume = currentVolume;
-    }
-
-    public int getMaxRadioStation() {
-        return maxRadioStation;
-    }
-
-    public void setMaxRadioStation(int maxRadioStation) {
-        this.maxRadioStation = maxRadioStation;
-    }
-
-    public int getMinRadioStation() {
-        return minRadioStation;
-    }
-
-    public void setMinRadioStation(int minRadioStation) {
-        this.minRadioStation = minRadioStation;
     }
 
     public int getCurrentRadioStation() {
